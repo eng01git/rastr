ae386db750654ae40fe9055c3757e1e7bbeceda1
eng01git <84606447+eng01git@users.noreply.github.com>
2021-08-21 10:43:57 -0300

    Update rastreabilidade.py

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -931,10 +931,9 @@ with col2:
 	else:
 		st.error('Ha paletes demais na reserva')
 	
-	fifo_in_show = df_ps_fifo_in.loc[df_ps_fifo_in['tipo_tampa'] == str(tipo_de_bobina)].sort_values(by='data_estoque', ascending=True)
-	fifo_in_show = fifo_in_show['numero_palete', 'tipo_tampa']
+	fifo_in_show = df_ps_fifo_in.sort_values(by='data_estoque', ascending=True)[['numero_palete', 'tipo_tampa']]
 	fifo_in_show.rename(columns={'numero_palete': 'Gerados'}, inplace=True)
-    
+
 	gridOptions, grid_height, return_mode_value, update_mode_value, fit_columns_on_grid_load, enable_enterprise_modules = config_grid(175, fifo_in_show, 0, 0, True)
 	response = AgGrid(
 		fifo_in_show,
