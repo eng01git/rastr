2bf5734dc43ced77e4f68cd5a9ff03a47226a7d8
eng01git <ambev.eng01@gmail.com>
2021-08-21 16:37:04 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -951,9 +951,10 @@ if df_bobinas.shape[0] > 0:
 			if con_palete_sem:
 				# observa o indice do primeiro elemento do fifo
 				#numero_palete = df_pal_sem.loc[(df_pal_sem['data_estoque'] != '-') & (df_pal_sem['data_consumo'] == '-'), 'numero_palete'].min()
-				st.write(df_pal_sem.loc[(df_pal_sem['data_estoque'] != '-') & (df_pal_sem['data_consumo'] == '-'), 'numero_palete'].min())
-				numero_palete = ps_fifo_in.sort_values(by='data_estoque', ascending=True).iloc[0]
-				st.write(numero_palete.iloc[7])
+				#st.write(df_pal_sem.loc[(df_pal_sem['data_estoque'] != '-') & (df_pal_sem['data_consumo'] == '-'), 'numero_palete'].min())
+				
+				numero_palete_aux = ps_fifo_in.sort_values(by='data_estoque', ascending=True).iloc[0]
+				numero_palete = numero_palete_aux.iloc[7]
 
 				# atualiza a data de consumo do palete consumido
 				df_pal_sem.loc[(df_pal_sem['numero_palete'] == numero_palete), 'data_consumo'] = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
