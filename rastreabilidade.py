71d6168446fc61dd22af336624cc84edf9cb35d5
eng01git <ambev.eng01@gmail.com>
2021-08-21 11:13:53 -0300

    commit

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -47,7 +47,7 @@ tz = pytz.timezone('America/Bahia')
 #												  funcoes															 #
 #######################################################################################################################
 
-
+st.write('teste')
 def trata_dados(data, tipo):
 	
 	# tratamento da planilha de tampas prata
