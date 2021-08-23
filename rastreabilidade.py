7d595e39e36ad847c6ddad53da7b8a9ad4607f6a
eng01git <ambev.eng01@gmail.com>
2021-08-23 11:05:37 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -1297,7 +1297,7 @@ if bobina_em_uso.shape[0] > 0:
 			st.write(paletes_produzidos)
 
 			st.write(df_pal_sem.loc[df_pal_sem['numero_OT'] == val_em_uso])
-			df_pal_sem.drop([(df_pal_sem['numero_OT'] == val_em_uso) & (df_pal_sem['documento'] >= paletes_produzidos)])
+			df_pal_sem.drop(df_pal_sem.loc[(df_pal_sem['numero_OT'] == val_em_uso) & (df_pal_sem['documento'] >= paletes_produzidos)].index)
 			#st.write(df_pal_sem.loc[(df_pal_sem['numero_OT'] == val_em_uso) & (df_pal_sem['documento'] >= paletes_produzidos)])
 			st.write(df_pal_sem.loc[df_pal_sem['numero_OT'] == val_em_uso])
 			#data.paletes_gerados = data.paletes_gerados.astype('int')
