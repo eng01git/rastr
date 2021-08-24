d6816714828259e35a5867897f082f7b3fd7b572
eng01git <ambev.eng01@gmail.com>
2021-08-24 15:15:58 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -1481,36 +1481,36 @@ with st.beta_expander('Análise de bobinas e selante por dia'):
 			for items in unicos:
 				st.info(items)
 
-			# 	# prepara dados para escrever no banco
-			# 	dic_sap = {}
-			# 	dic_sap = df_bobinas.loc[(df_bobinas['numero_OT'] == val_em_uso)].to_dict('records')
+				# prepara dados para escrever no banco
+				dic_sap = {}
+				dic_sap = df_bobinas.loc[(df_bobinas['numero_OT'] == items)].to_dict('records')
 
-			# 	# Transforma dados do formulário em um dicionário
-			# 	keys_values = dic_remove[0].items()
-			# 	new_remove = {str(key): str(value) for key, value in keys_values}
-			# 	documento_remove = new_remove['numero_OT']
+				# Transforma dados do formulário em um dicionário
+				keys_values = dic_sap[0].items()
+				new_sap = {str(key): str(value) for key, value in keys_values}
+				documento_sap = new_sap['numero_OT']
 
-			# 	# escreve o dataframe dos paletes na selante para escrita em banco (não altera valor, mas escreve para não perder os dados)
-			# 	new_remove['Paletes'] = df_pal_sem.loc[(df_pal_sem['numero_OT'] == val_em_uso)].to_csv()
+				# escreve o dataframe dos paletes na selante para escrita em banco (não altera valor, mas escreve para não perder os dados)
+				new_sap['Paletes'] = df_pal_sem.loc[(df_pal_sem['numero_OT'] == items)].to_csv()
 
-			# 	# flag para rodar novamente o script
-			# 	rerun = False
+				# flag para rodar novamente o script
+				rerun = False
 
-			# 	# Armazena no banco as alteracoes da bobina
-			# 	try:
-			# 		doc_ref = db.collection("Bobina").document(documento_remove)
-			# 		doc_ref.set(new_remove)
-			# 		st.success('Modificação armazenada com sucesso!')
-			# 		rerun = True
-			# 	except:
-			# 		st.error('Falha ao armazenar modificação, tente novamente ou entre em contato com suporte!')
-			# 		caching.clear_cache()
+				# Armazena no banco as alteracoes da bobina
+				try:
+					doc_ref = db.collection("Bobina").document(documento_sap)
+					doc_ref.set(new_sap)
+					st.success('Modificação armazenada com sucesso!')
+					rerun = True
+				except:
+					st.error('Falha ao armazenar modificação, tente novamente ou entre em contato com suporte!')
+					caching.clear_cache()
 
-			# 	# comando para rodar novament o script
-			# 	if rerun:
-			# 		st.experimental_rerun()
+				# comando para rodar novament o script
+				if rerun:
+					st.experimental_rerun()
 
-			# 	st.write(df_pal_sem)
+				st.write(df_pal_sem)
 	else:
 		st.error('Não há paletes para serem apontados para data selecionada')
 
