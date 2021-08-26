7bd3f96683e4d4522dd9d009da2e7b9040fce984
eng01git <ambev.eng01@gmail.com>
2021-08-26 13:22:47 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -1440,7 +1440,7 @@ if telas == 'Detalhamento de bobinas e selantes por data':
 			# filtra as bobinas de acordo com a data
 			filtro_bobina = bobinas_filtradas.loc[(bobinas_filtradas['data_entrada'].dt.date == data_filtro)]
 			filtro_bobina_s = bobinas_filtradas_s.loc[bobinas_filtradas_s['data_saida'].dt.time == data_filtro]
-			filtro_bobina_s['data_saida'] = filtro_bobina_s['data_saida'].dt.strftime("%H:%M %d/%m/%Y")
+			filtro_bobina_s['data_saida'] = filtro_bobina_s['data_saida'].dt.strftime("%H:%M %d/%m/%Y") 
 			
 			if (filtro_bobina.shape[0] > 0) or (filtro_bobina_s.shape[0] > 0):
 				
