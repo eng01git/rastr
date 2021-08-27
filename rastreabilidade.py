6d2ac2cd589b69e089a9d0cb3928ac0f12edde48
eng01git <ambev.eng01@gmail.com>
2021-08-27 11:06:32 -0300

    zxdcfgh

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -66,7 +66,7 @@ def upload_excel_selante(uploaded_file):
 	df.data = datetime.now(tz).strftime("%H:%M %d-%m-%Y")
 	df.data_entrada = '-'
 	df.data_saida = '-'
-	df.paletes_gerados = (df['peso_vedante']) * 412 / 187200
+	df.paletes_gerados = (df['peso_vedante']) * 2857 / 187200
 	df.paletes_gerados = df.paletes_gerados.astype('int')
 	df.status = 'Disponível'
 	df['comentario'] = '-'
