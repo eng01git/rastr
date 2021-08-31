32c27f86f6e77b815b08cf03779803c110abe4ec
eng01git <ambev.eng01@gmail.com>
2021-08-31 09:01:38 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -1571,7 +1571,7 @@ if telas == 'Detalhamento de bobinas e selantes por data':
 			selantes_filtradas_s['data_saida'] = pd.to_datetime(selantes_filtradas_s['data_saida'])
 
 			# filtra as selantes de acordo com a data
-			filtro_selante = selantes_filtradas.loc[(selantes_filtradas['data_entrada'].dt.date <= data_filtro) & (selantes_filtradas_s['data_saida'].dt.date == '-')]
+			filtro_selante = selantes_filtradas.loc[(selantes_filtradas['data_entrada'].dt.date <= data_filtro) & (selantes_filtradas['data_saida'].dt.date == '-')]
 			st.write(filtro_selante)
 			filtro_selante_s = selantes_filtradas_s.loc[(selantes_filtradas['data_entrada'].dt.date <= data_filtro) & (selantes_filtradas_s['data_saida'].dt.date >= data_filtro)]
 			st.write(filtro_selante_s)
