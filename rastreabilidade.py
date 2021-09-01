e588d11824a1fbb49c705a4d49d0d164ff238b39
eng01git <ambev.eng01@gmail.com>
2021-09-01 14:22:32 -0300

    asd

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -305,7 +305,7 @@ def download_etiqueta(data, tipo): # 0 sem selante e 1 com selante
 	else:
 		ws['B11'] = 'C'  # 'turno'
 
-	data_atual = datetime.date(data['data_estoque']) #.strftime("%m/%d/%Y")
+	data_atual = datetime.date(data['data_estoque']).strftime("%m/%d/%Y")
 	st.write(data_atual)
 	ws['A11'] = data_atual #data['data_estoque']  # 'data'
 	ws['C11'] = data['data_estoque']  # 'hora'
