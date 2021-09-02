585f8b94d5576d628643a95ecd56fad8c8ad8f02
eng01git <ambev.eng01@gmail.com>
2021-09-02 11:18:13 -0300

    ad

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -1664,7 +1664,7 @@ if telas == 'Apontamento de código SAP':
 	
 	st.write(df_pal_sem_filtrado['data_estoque'].dtype)
 	df_pal_sem_filtrado['data_estoque'] = pd.to_datetime(df_pal_sem_filtrado['data_estoque'])
-	df_pal_sem_filtrado['data_estoque'] = df_pal_sem_filtrado['data_estoque'].strftime('%Y-%m-%d %H:%M:%S')
+	df_pal_sem_filtrado['data_estoque'] = df_pal_sem_filtrado['data_estoque'].dt.strftime('%Y-%m-%d %H:%M:%S')
 	st.write(df_pal_sem_filtrado['data_estoque'])
 
 	# filtra pela data selecionada
