6a3c4d38d193d56c3b6902e2a4c5f0a9404b64e7
Mario Carvalho <marius.lisboa@gmail.com>
2022-07-01 10:33:47 -0300

    Inserindo conversion

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -958,7 +958,7 @@ with st.expander('Gerenciamento de bobinas da Conversion'):
 
 	st.subheader('Bobinas em uso nas Conversions')
 	if df_bobinas.shape[0] > 0:
-		gridOptions, grid_height, return_mode_value, update_mode_value, fit_columns_on_grid_load, enable_enterprise_modules = config_grid(50, df, 0, 0, True)
+		gridOptions, grid_height, return_mode_value, update_mode_value, fit_columns_on_grid_load, enable_enterprise_modules = config_grid(60, df, 0, 0, True)
 		
 		response = AgGrid(
 			df[df['Conversion'] == '1'].head(1),
