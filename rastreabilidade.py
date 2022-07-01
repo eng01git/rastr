7cb681e118cbc90053be326da8e7ac47f61849ac
Mario Carvalho <marius.lisboa@gmail.com>
2022-07-01 10:03:21 -0300

    Inserindo conversion

--- rastreabilidade.py
+++ rastreabilidade.py
@@ -673,13 +673,13 @@ def adicionar_bobina_conversion(df: pd.DataFrame):
 		dic['Strokes'] = s2.number_input('Quantidade de strokes', min_value=stroke_min)
 		dic['peso_bobina'] = s3.number_input('Peso da bobina', step=100, format='%i', value=5000, max_value=10000)
 		dic['data_saida'] = '-'
-		submitted = s5.form_submit_button('Adicionar selante ao sistema')
+		submitted = s5.form_submit_button('Adicionar bobina ao sistema')
 
 	if submitted:
 		if df_bobinas_conversion[df_bobinas_conversion['Numero_ot'] == (dic['Numero_ot'])].shape[0] == 0:
 
-			df_data_setup_new = pd.DataFrame([[dic['Conversion'], dic['data_entrada'], dic['Numero_OT'], dic['strokes'], dic['peso_bobina'], dic['data_saida']]], 
-											columns=['Conversion', 'data_entrada', 'Numero_OT', 'strokes', 'peso_bobina', 'data_saida'])
+			df_data_setup_new = pd.DataFrame([[dic['Conversion'], dic['data_entrada'], dic['Numero_ot'], dic['strokes'], dic['peso_bobina'], dic['data_saida']]], 
+											columns=['Conversion', 'data_entrada', 'Numero_ot', 'strokes', 'peso_bobina', 'data_saida'])
 
 			df_bobinas_conversion = df_bobinas_conversion.append(df_data_setup_new)
 			rerun = False
